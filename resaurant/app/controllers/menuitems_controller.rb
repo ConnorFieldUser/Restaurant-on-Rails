62c1d21fd64f923466f7b8a63541e3f27d7467b0
ConnorFieldUser <conquest.field@gmail.com>
2017-01-27 14:46:22 -0500

    form working

--- resaurant/app/controllers/menuitems_controller.rb
+++ resaurant/app/controllers/menuitems_controller.rb
@@ -1,9 +1,23 @@
 class MenuitemsController < ApplicationController
+
+  def show
+    @menuitem = Menuitem.find(params[:id])
+  end
+
   def new
   end
 
   def create
-    render plain: params[:menuitem].inspect
+    @menuitem = Menuitem.new(menuitem_params)
+
+    @menuitem.save
+    redirect_to @menuitem
+
   end
+
+  private
+    def menuitem_params
+      params.require(:menuitem).permit(:name, :description, :ready, :served)
+    end
 
 end
